410d5bd96346fdceb54068c8ae93e07bd9010b4b
HARUSAKI7200 <rikujou0702@gmail.com>
2025-10-24 20:04:51 +0900

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,64 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+import java.util.Properties
+import java.io.FileInputStream
+
+fun localProperties(): Properties {
+    val localPropertiesFile = rootProject.file("local.properties")
+    val properties = Properties()
+    if (localPropertiesFile.exists()) {
+        properties.load(FileInputStream(localPropertiesFile))
+    }
+    return properties
+}
+
+// ▼▼▼ 修正点：この2行を以下に置き換えてください ▼▼▼
+val flutterVersionCode = localProperties().getProperty("flutter.versionCode")
+val flutterVersionName = localProperties().getProperty("flutter.versionName")
+
+android {
+    namespace = "com.example.nifuda_gpt_app_fixed"
+    compileSdk = 35
+
+    ndkVersion = "27.0.12077973"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    sourceSets {
+        getByName("main") {
+            java.srcDirs("src/main/kotlin")
+        }
+    }
+
+    defaultConfig {
+        applicationId = "com.example.shogo_app"
+        minSdk = 21
+        targetSdk = 35
+        // プロパティがNullでないことを確認
+        versionCode = flutterVersionCode?.toInt() ?: 1
+        versionName = flutterVersionName
+    }
+
+    buildTypes {
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
+
+dependencies {}
